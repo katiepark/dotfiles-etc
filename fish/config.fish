a8f8f99a8cc995f9963a2bdeba17ed2161f7a9de
Katie Park <katie.e.park@gmail.com>
2024-11-06 22:24:31 -0500

    Clean up path, remove pyenv init and speed up fish startup time a ton

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,10 @@
 source ~/.config/fish/aliases.fish
 
 # Load rbenv and set up path
-set PATH /Users/admin/.rbenv/shims $PATH
+fish_add_path /Users/admin/.rbenv/shims
 status --is-interactive; and . (rbenv init -|psub)
 
 # Set up fzf key bindings
 fzf --fish | source
 # Override fzf default command to use ripgrep (excludes files in .gitignore)
 export FZF_DEFAULT_COMMAND='rg --files'
-
-# Init pyenv
-pyenv init - | source
